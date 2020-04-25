885ec0a235815bd86fbdd8c08968a291a2fd6a54
fantasy1022 <fantasy-1022@hotmail.com>
2020-04-25 15:39:18 +0800

    Upgrade to viewpager2.

--- app/src/main/java/com/fantasyfang/developmenttool/MainActivity.kt
+++ app/src/main/java/com/fantasyfang/developmenttool/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.fantasyfang.developmenttool
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import com.fantasyfang.developmenttool.databinding.ActivityMainBinding
 import com.fantasyfang.developmenttool.ui.main.SectionsPagerAdapter
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,13 +16,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
 
-        binding.viewPager.adapter = sectionsPagerAdapter
-        binding.tabs.setupWithViewPager(binding.viewPager)
+        binding.viewPager.adapter = SectionsPagerAdapter(this)
+        TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
 }
+
+private val TAB_TITLES = arrayOf(
+    R.string.tab_text_screen,
+    R.string.tab_text_device
+)
