4c9e5a895084f7b6839fa157787ac470ddc557cc
fantasy1022 <fantasy-1022@hotmail.com>
2020-05-27 21:49:50 +0800

    Add device items

--- app/src/main/java/com/fantasyfang/developmenttool/data/DeviceInfo.kt
+++ app/src/main/java/com/fantasyfang/developmenttool/data/DeviceInfo.kt
@@ -9,5 +9,24 @@ data class DeviceInfo(val item: Item<DeviceItem, String>) : InfoBase {
 }
 
 enum class DeviceItem(val value: Int) {
-    DEVICE_NAME(R.string.device_name)
+    MODEL(R.string.device_model),
+    ANDROID_VERSION_CODE(R.string.device_android_version_code),
+    ANDROID_VERSION(R.string.device_android_version),
+    BOARD(R.string.device_board),
+    BOOTLOADER(R.string.device_bootloader),
+    BRAND(R.string.device_brand),
+    SUPPORTED_ABIS(R.string.device_supported_abis),
+    DEVICE(R.string.device_device),
+    DISPLAY(R.string.device_display),
+    FINGERPRINT(R.string.device_fingerprint),
+    HARDWARE(R.string.device_hardware),
+    HOST(R.string.device_host),
+    ID(R.string.device_id),
+    MANUFACTURER(R.string.device_manufacturer),
+    PRODUCT(R.string.device_product),
+    SUPPORTED_32_BIT_ABIS(R.string.device_supported_32_abis),
+    SUPPORTED_64_BIT_ABIS(R.string.device_supported_64_abis),
+    TAGS(R.string.device_tags),
+    TYPE(R.string.device_type),
+    USER(R.string.device_user)
 }
--- app/src/main/java/com/fantasyfang/developmenttool/repository/device/DeviceInfoLocalDataStore.kt
+++ app/src/main/java/com/fantasyfang/developmenttool/repository/device/DeviceInfoLocalDataStore.kt
@@ -1,5 +1,6 @@
 package com.fantasyfang.developmenttool.repository.device
 
+import android.os.Build
 import com.fantasyfang.developmenttool.data.DeviceInfo
 import com.fantasyfang.developmenttool.data.DeviceItem
 import com.fantasyfang.developmenttool.data.Item
@@ -9,10 +10,42 @@ import javax.inject.Inject
 class DeviceInfoLocalDataStore @Inject constructor() : LocalDataStore<List<DeviceInfo>> {
 
     override fun getData(): List<DeviceInfo> {
-        val build = android.os.Build.DISPLAY
-        return listOf(
-            DeviceInfo(Item(DeviceItem.DEVICE_NAME, android.os.Build.MODEL))
-        )
+        val build = Build.DISPLAY
+        val supportABI = Build.SUPPORTED_ABIS.reduce { sum, element -> "$sum, $element" }
+        val support32ABI = Build.SUPPORTED_32_BIT_ABIS.reduce { sum, element -> "$sum, $element" }
+        val support64ABI = Build.SUPPORTED_64_BIT_ABIS.reduce { sum, element -> "$sum, $element" }
 
+        return listOf(
+            DeviceInfo(Item(DeviceItem.MODEL, Build.MODEL)),
+            DeviceInfo(
+                Item(
+                    DeviceItem.ANDROID_VERSION_CODE,
+                    Build.VERSION.SDK_INT.toString()
+                )
+            ),
+            DeviceInfo(
+                Item(
+                    DeviceItem.ANDROID_VERSION,
+                    Build.VERSION.RELEASE
+                )
+            ),
+            DeviceInfo(Item(DeviceItem.BOARD, Build.BOARD)),
+            DeviceInfo(Item(DeviceItem.BOOTLOADER, Build.BOOTLOADER)),
+            DeviceInfo(Item(DeviceItem.BRAND, Build.BRAND)),
+            DeviceInfo(Item(DeviceItem.SUPPORTED_ABIS, supportABI)),
+            DeviceInfo(Item(DeviceItem.DEVICE, Build.DEVICE)),
+            DeviceInfo(Item(DeviceItem.DISPLAY, Build.DISPLAY)),
+            DeviceInfo(Item(DeviceItem.FINGERPRINT, Build.FINGERPRINT)),
+            DeviceInfo(Item(DeviceItem.HARDWARE, Build.HARDWARE)),
+            DeviceInfo(Item(DeviceItem.HOST, Build.HOST)),
+            DeviceInfo(Item(DeviceItem.ID, Build.ID)),
+            DeviceInfo(Item(DeviceItem.MANUFACTURER, Build.MANUFACTURER)),
+            DeviceInfo(Item(DeviceItem.PRODUCT, Build.PRODUCT)),
+            DeviceInfo(Item(DeviceItem.SUPPORTED_32_BIT_ABIS, support32ABI)),
+            DeviceInfo(Item(DeviceItem.SUPPORTED_64_BIT_ABIS, support64ABI)),
+            DeviceInfo(Item(DeviceItem.TAGS, Build.TAGS)),
+            DeviceInfo(Item(DeviceItem.TYPE, Build.TYPE)),
+            DeviceInfo(Item(DeviceItem.USER, Build.USER))
+        )
     }
 }
