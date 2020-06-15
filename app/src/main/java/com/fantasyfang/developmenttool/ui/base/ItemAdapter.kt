b0a266255cc31eeba9fa0f03efe9330f8a7d82f4
fantasy1022 <fantasy-1022@hotmail.com>
2020-06-15 22:51:29 +0800

    Add error button in ItemAdapter.kt

--- app/src/main/java/com/fantasyfang/developmenttool/ui/base/ItemAdapter.kt
+++ app/src/main/java/com/fantasyfang/developmenttool/ui/base/ItemAdapter.kt
@@ -2,6 +2,7 @@ package com.fantasyfang.developmenttool.ui.base
 
 import android.content.res.Resources
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.fantasyfang.developmenttool.data.InfoBase
@@ -34,16 +35,29 @@ class ItemAdapter<T : InfoBase>(var listItems: List<T> = listOf()) :
 
         fun updateContent(t: T) {
             itemScreenBinding.titleText.text = res.getString(t.getStringId())
-            itemScreenBinding.valueText.text =
-                when (t.getValue()) {
-                    is Result.Success -> {
-                        (t.getValue() as Result.Success).string
-                    }
-                    is Result.FailureLackPermission -> {
-                        //TODO:Show button
-                        (t.getValue() as Result.FailureLackPermission).lackPermission.value
+            when (t.getValue()) {
+                is Result.Success -> {
+                    with(itemScreenBinding) {
+                        valueText.visibility = View.VISIBLE
+                        valueText.text = (t.getValue() as Result.Success).string
+                        errorButton.visibility = View.GONE
                     }
                 }
+                is Result.FailureLackPermission -> {
+                    with(itemScreenBinding) {
+                        valueText.visibility = View.GONE
+                        errorButton.visibility = View.VISIBLE
+                        //TODO: Parse permission name
+                        //TOOD: Use string with placeHolder
+                        errorButton.text =
+                            "Get ${(t.getValue() as Result.FailureLackPermission).lackPermission.value}"
+                        errorButton.setOnClickListener {
+                            //TODO: Use interface to bring permission
+                        }
+                    }
+
+                }
+            }
         }
     }
 
