cb4fcea58e5b8b85f27bd23eba34b137da68bae3
fantasy1022 <fantasy-1022@hotmail.com>
2020-07-04 17:41:05 +0800

    Hide imei function

--- app/src/main/java/com/fantasyfang/developmenttool/repository/id/IdInfoLocalDataStore.kt
+++ app/src/main/java/com/fantasyfang/developmenttool/repository/id/IdInfoLocalDataStore.kt
@@ -9,7 +9,6 @@ import com.fantasyfang.developmenttool.data.IdInfo
 import com.fantasyfang.developmenttool.data.IdItem
 import com.fantasyfang.developmenttool.data.Item
 import com.fantasyfang.developmenttool.extension.getAndroidId
-import com.fantasyfang.developmenttool.extension.getDeviceIMEI
 import com.fantasyfang.developmenttool.extension.getIPV4Address
 import com.fantasyfang.developmenttool.repository.LocalDataStore
 import javax.inject.Inject
@@ -23,7 +22,7 @@ class IdInfoLocalDataStore @Inject constructor(private val context: Context) :
             emit(
                 listOf(
                     IdInfo(Item(IdItem.ANDROID_ID, context.getAndroidId())),
-                    IdInfo(Item(IdItem.IMEI, context.getDeviceIMEI())),
+//                    IdInfo(Item(IdItem.IMEI, context.getDeviceIMEI())),
                     IdInfo(Item(IdItem.LOCAL_IP, context.getIPV4Address()))
                 )
             )
